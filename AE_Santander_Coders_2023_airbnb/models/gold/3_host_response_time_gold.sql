4698ced5cf0e6a891ff3fb29460fa34009153617
Everton Donato Cavalcante <evdcavalcante@intervalor.com.br>
2024-04-29 19:15:12 -0300

    Adição de novas visões na camada gold

--- AE_Santander_Coders_2023_airbnb/models/gold/3_host_response_time_gold.sql
+++ AE_Santander_Coders_2023_airbnb/models/gold/3_host_response_time_gold.sql
@@ -6,9 +6,9 @@ final as (
     select host_response_time
         , COUNT(*) AS response_count
     from dados_listings_silver 
+    where host_response_time is not null
     group by host_response_time
     order by host_response_time
-    where host_response_time is not null
 )
 
 select * from final
--- AE_Santander_Coders_2023_airbnb/models/gold/3_neighborhood_avarage_prices_gold.sql
+++ AE_Santander_Coders_2023_airbnb/models/gold/3_neighborhood_avarage_prices_gold.sql
@@ -1,13 +1,12 @@
 with dados_listings_silver as (
     select * from {{ source('dados_silver', '2_listings_silver') }}
 ),
-
 final as (
     select neighbourhood_cleansed
         , AVG(price) AS avg_price
-    from dados_reviews_silver
-    group by neighbourhood
+    from dados_listings_silver
     where price is not null
+    group by neighbourhood_cleansed
 )
 
 select * from final
